3b7e2d4b5a764069838845b01d8336fbe5d82b70
Braden Donaldson <Braden@Bradens-MBPro.local>
2020-08-20 11:06:10 -0700

    set up router

---
+++ lib/presentation/routes/router.dart
@@ -0,0 +1,7 @@
+import 'package:auto_route/auto_route_annotations.dart';
+import 'package:shattered_star/presentation/sign_in/sign_in_page.dart';
+
+@MaterialAutoRouter(generateNavigationHelperExtension: true)
+class $Router {
+  SignInPage signInPage;
+}
