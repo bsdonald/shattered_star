55463c724a28ee22a33a80fbf9408f32843a9d77
Braden Donaldson <Braden@Bradens-MBPro.local>
2020-08-20 11:29:17 -0700

    set up splash page

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -1,20 +1,36 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shattered_star/application/auth/auth_bloc.dart';
+import 'package:shattered_star/injection.dart';
+import 'package:shattered_star/presentation/routes/router.gr.dart';
 import 'package:shattered_star/presentation/sign_in/sign_in_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Shattered Star Player's Guide",
-      home: SignInPage(),
-      theme: ThemeData.light().copyWith(
-        primaryColor: Colors.lightBlueAccent[100],
-        // primaryColor: Colors.orange[200],
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => getIt<AuthBloc>()..add(const AuthEvent.authCheckRequested()),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Shattered Star Player's Guide",
+        builder: ExtendedNavigator(
+          router: Router(),
+        ),
+        theme: ThemeData.light().copyWith(
+          primaryColor: Colors.lightBlueAccent[100],
+          // primaryColor: Colors.orange[200],
 
-        accentColor: Colors.orangeAccent,
-        inputDecorationTheme: InputDecorationTheme(
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(8),),
+          accentColor: Colors.orangeAccent,
+          inputDecorationTheme: InputDecorationTheme(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
         ),
       ),
     );
---
+++ lib/presentation/splash/splash_page.dart
@@ -0,0 +1,29 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shattered_star/application/auth/auth_bloc.dart';
+import 'package:shattered_star/presentation/routes/router.gr.dart';
+
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        state.map(
+          initial: (_) {},
+          authenticated: (_) {
+            print('Authenticated');
+          },
+          unauthenticated: (_) => ExtendedNavigator.of(context).pushReplacementNamed(Routes.signInPage),
+        );
+      },
+      child: const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
+
+// => ExtendedNavigator.of(context).pushReplacementNamed(Routes.)
