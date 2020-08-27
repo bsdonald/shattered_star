15b6c8377b6043dbd63afe5aa4813f83db511298
Braden Donaldson <bsdonald7@gmail.com>
2020-08-27 13:21:32 -0700

    set up actor and watcher blocs

---
+++ lib/application/notes/note_watcher/note_watcher_bloc.freezed.dart
@@ -0,0 +1,136 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named
+
+part of 'note_watcher_bloc.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+class _$NoteWatcherEventTearOff {
+  const _$NoteWatcherEventTearOff();
+
+  _Started started() {
+    return const _Started();
+  }
+}
+
+// ignore: unused_element
+const $NoteWatcherEvent = _$NoteWatcherEventTearOff();
+
+mixin _$NoteWatcherEvent {}
+
+abstract class $NoteWatcherEventCopyWith<$Res> {
+  factory $NoteWatcherEventCopyWith(
+          NoteWatcherEvent value, $Res Function(NoteWatcherEvent) then) =
+      _$NoteWatcherEventCopyWithImpl<$Res>;
+}
+
+class _$NoteWatcherEventCopyWithImpl<$Res>
+    implements $NoteWatcherEventCopyWith<$Res> {
+  _$NoteWatcherEventCopyWithImpl(this._value, this._then);
+
+  final NoteWatcherEvent _value;
+  // ignore: unused_field
+  final $Res Function(NoteWatcherEvent) _then;
+}
+
+abstract class _$StartedCopyWith<$Res> {
+  factory _$StartedCopyWith(_Started value, $Res Function(_Started) then) =
+      __$StartedCopyWithImpl<$Res>;
+}
+
+class __$StartedCopyWithImpl<$Res> extends _$NoteWatcherEventCopyWithImpl<$Res>
+    implements _$StartedCopyWith<$Res> {
+  __$StartedCopyWithImpl(_Started _value, $Res Function(_Started) _then)
+      : super(_value, (v) => _then(v as _Started));
+
+  @override
+  _Started get _value => super._value as _Started;
+}
+
+class _$_Started implements _Started {
+  const _$_Started();
+
+  @override
+  String toString() {
+    return 'NoteWatcherEvent.started()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) || (other is _Started);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+}
+
+abstract class _Started implements NoteWatcherEvent {
+  const factory _Started() = _$_Started;
+}
+
+class _$NoteWatcherStateTearOff {
+  const _$NoteWatcherStateTearOff();
+
+  _Initial initial() {
+    return const _Initial();
+  }
+}
+
+// ignore: unused_element
+const $NoteWatcherState = _$NoteWatcherStateTearOff();
+
+mixin _$NoteWatcherState {}
+
+abstract class $NoteWatcherStateCopyWith<$Res> {
+  factory $NoteWatcherStateCopyWith(
+          NoteWatcherState value, $Res Function(NoteWatcherState) then) =
+      _$NoteWatcherStateCopyWithImpl<$Res>;
+}
+
+class _$NoteWatcherStateCopyWithImpl<$Res>
+    implements $NoteWatcherStateCopyWith<$Res> {
+  _$NoteWatcherStateCopyWithImpl(this._value, this._then);
+
+  final NoteWatcherState _value;
+  // ignore: unused_field
+  final $Res Function(NoteWatcherState) _then;
+}
+
+abstract class _$InitialCopyWith<$Res> {
+  factory _$InitialCopyWith(_Initial value, $Res Function(_Initial) then) =
+      __$InitialCopyWithImpl<$Res>;
+}
+
+class __$InitialCopyWithImpl<$Res> extends _$NoteWatcherStateCopyWithImpl<$Res>
+    implements _$InitialCopyWith<$Res> {
+  __$InitialCopyWithImpl(_Initial _value, $Res Function(_Initial) _then)
+      : super(_value, (v) => _then(v as _Initial));
+
+  @override
+  _Initial get _value => super._value as _Initial;
+}
+
+class _$_Initial implements _Initial {
+  const _$_Initial();
+
+  @override
+  String toString() {
+    return 'NoteWatcherState.initial()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) || (other is _Initial);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+}
+
+abstract class _Initial implements NoteWatcherState {
+  const factory _Initial() = _$_Initial;
+}
