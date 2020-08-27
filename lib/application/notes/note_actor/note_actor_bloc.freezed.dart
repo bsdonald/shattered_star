d64aed9e5d596e7a69655152b3274fb28118caab
Braden Donaldson <bsdonald7@gmail.com>
2020-08-27 16:54:09 -0700

    set up actor events

--- lib/application/notes/note_actor/note_actor_bloc.freezed.dart
+++ lib/application/notes/note_actor/note_actor_bloc.freezed.dart
@@ -78,12 +78,57 @@ class _$NoteActorStateTearOff {
   _Initial initial() {
     return const _Initial();
   }
+
+  _ActionInProgress actionInProgress() {
+    return const _ActionInProgress();
+  }
+
+  _DeleteFailure deleteFailure(NoteFailure noteFailure) {
+    return _DeleteFailure(
+      noteFailure,
+    );
+  }
+
+  _DeleteSucess deleteSuccess() {
+    return const _DeleteSucess();
+  }
 }
 
 // ignore: unused_element
 const $NoteActorState = _$NoteActorStateTearOff();
 
-mixin _$NoteActorState {}
+mixin _$NoteActorState {
+  @optionalTypeArgs
+  Result when<Result extends Object>({
+    @required Result initial(),
+    @required Result actionInProgress(),
+    @required Result deleteFailure(NoteFailure noteFailure),
+    @required Result deleteSuccess(),
+  });
+  @optionalTypeArgs
+  Result maybeWhen<Result extends Object>({
+    Result initial(),
+    Result actionInProgress(),
+    Result deleteFailure(NoteFailure noteFailure),
+    Result deleteSuccess(),
+    @required Result orElse(),
+  });
+  @optionalTypeArgs
+  Result map<Result extends Object>({
+    @required Result initial(_Initial value),
+    @required Result actionInProgress(_ActionInProgress value),
+    @required Result deleteFailure(_DeleteFailure value),
+    @required Result deleteSuccess(_DeleteSucess value),
+  });
+  @optionalTypeArgs
+  Result maybeMap<Result extends Object>({
+    Result initial(_Initial value),
+    Result actionInProgress(_ActionInProgress value),
+    Result deleteFailure(_DeleteFailure value),
+    Result deleteSuccess(_DeleteSucess value),
+    @required Result orElse(),
+  });
+}
 
 abstract class $NoteActorStateCopyWith<$Res> {
   factory $NoteActorStateCopyWith(
@@ -129,8 +174,407 @@ class _$_Initial implements _Initial {
 
   @override
   int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  Result when<Result extends Object>({
+    @required Result initial(),
+    @required Result actionInProgress(),
+    @required Result deleteFailure(NoteFailure noteFailure),
+    @required Result deleteSuccess(),
+  }) {
+    assert(initial != null);
+    assert(actionInProgress != null);
+    assert(deleteFailure != null);
+    assert(deleteSuccess != null);
+    return initial();
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeWhen<Result extends Object>({
+    Result initial(),
+    Result actionInProgress(),
+    Result deleteFailure(NoteFailure noteFailure),
+    Result deleteSuccess(),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (initial != null) {
+      return initial();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  Result map<Result extends Object>({
+    @required Result initial(_Initial value),
+    @required Result actionInProgress(_ActionInProgress value),
+    @required Result deleteFailure(_DeleteFailure value),
+    @required Result deleteSuccess(_DeleteSucess value),
+  }) {
+    assert(initial != null);
+    assert(actionInProgress != null);
+    assert(deleteFailure != null);
+    assert(deleteSuccess != null);
+    return initial(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeMap<Result extends Object>({
+    Result initial(_Initial value),
+    Result actionInProgress(_ActionInProgress value),
+    Result deleteFailure(_DeleteFailure value),
+    Result deleteSuccess(_DeleteSucess value),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (initial != null) {
+      return initial(this);
+    }
+    return orElse();
+  }
 }
 
 abstract class _Initial implements NoteActorState {
   const factory _Initial() = _$_Initial;
+}
+
+abstract class _$ActionInProgressCopyWith<$Res> {
+  factory _$ActionInProgressCopyWith(
+          _ActionInProgress value, $Res Function(_ActionInProgress) then) =
+      __$ActionInProgressCopyWithImpl<$Res>;
+}
+
+class __$ActionInProgressCopyWithImpl<$Res>
+    extends _$NoteActorStateCopyWithImpl<$Res>
+    implements _$ActionInProgressCopyWith<$Res> {
+  __$ActionInProgressCopyWithImpl(
+      _ActionInProgress _value, $Res Function(_ActionInProgress) _then)
+      : super(_value, (v) => _then(v as _ActionInProgress));
+
+  @override
+  _ActionInProgress get _value => super._value as _ActionInProgress;
+}
+
+class _$_ActionInProgress implements _ActionInProgress {
+  const _$_ActionInProgress();
+
+  @override
+  String toString() {
+    return 'NoteActorState.actionInProgress()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) || (other is _ActionInProgress);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  Result when<Result extends Object>({
+    @required Result initial(),
+    @required Result actionInProgress(),
+    @required Result deleteFailure(NoteFailure noteFailure),
+    @required Result deleteSuccess(),
+  }) {
+    assert(initial != null);
+    assert(actionInProgress != null);
+    assert(deleteFailure != null);
+    assert(deleteSuccess != null);
+    return actionInProgress();
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeWhen<Result extends Object>({
+    Result initial(),
+    Result actionInProgress(),
+    Result deleteFailure(NoteFailure noteFailure),
+    Result deleteSuccess(),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (actionInProgress != null) {
+      return actionInProgress();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  Result map<Result extends Object>({
+    @required Result initial(_Initial value),
+    @required Result actionInProgress(_ActionInProgress value),
+    @required Result deleteFailure(_DeleteFailure value),
+    @required Result deleteSuccess(_DeleteSucess value),
+  }) {
+    assert(initial != null);
+    assert(actionInProgress != null);
+    assert(deleteFailure != null);
+    assert(deleteSuccess != null);
+    return actionInProgress(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeMap<Result extends Object>({
+    Result initial(_Initial value),
+    Result actionInProgress(_ActionInProgress value),
+    Result deleteFailure(_DeleteFailure value),
+    Result deleteSuccess(_DeleteSucess value),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (actionInProgress != null) {
+      return actionInProgress(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _ActionInProgress implements NoteActorState {
+  const factory _ActionInProgress() = _$_ActionInProgress;
+}
+
+abstract class _$DeleteFailureCopyWith<$Res> {
+  factory _$DeleteFailureCopyWith(
+          _DeleteFailure value, $Res Function(_DeleteFailure) then) =
+      __$DeleteFailureCopyWithImpl<$Res>;
+  $Res call({NoteFailure noteFailure});
+
+  $NoteFailureCopyWith<$Res> get noteFailure;
+}
+
+class __$DeleteFailureCopyWithImpl<$Res>
+    extends _$NoteActorStateCopyWithImpl<$Res>
+    implements _$DeleteFailureCopyWith<$Res> {
+  __$DeleteFailureCopyWithImpl(
+      _DeleteFailure _value, $Res Function(_DeleteFailure) _then)
+      : super(_value, (v) => _then(v as _DeleteFailure));
+
+  @override
+  _DeleteFailure get _value => super._value as _DeleteFailure;
+
+  @override
+  $Res call({
+    Object noteFailure = freezed,
+  }) {
+    return _then(_DeleteFailure(
+      noteFailure == freezed ? _value.noteFailure : noteFailure as NoteFailure,
+    ));
+  }
+
+  @override
+  $NoteFailureCopyWith<$Res> get noteFailure {
+    if (_value.noteFailure == null) {
+      return null;
+    }
+    return $NoteFailureCopyWith<$Res>(_value.noteFailure, (value) {
+      return _then(_value.copyWith(noteFailure: value));
+    });
+  }
+}
+
+class _$_DeleteFailure implements _DeleteFailure {
+  const _$_DeleteFailure(this.noteFailure) : assert(noteFailure != null);
+
+  @override
+  final NoteFailure noteFailure;
+
+  @override
+  String toString() {
+    return 'NoteActorState.deleteFailure(noteFailure: $noteFailure)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _DeleteFailure &&
+            (identical(other.noteFailure, noteFailure) ||
+                const DeepCollectionEquality()
+                    .equals(other.noteFailure, noteFailure)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^ const DeepCollectionEquality().hash(noteFailure);
+
+  @override
+  _$DeleteFailureCopyWith<_DeleteFailure> get copyWith =>
+      __$DeleteFailureCopyWithImpl<_DeleteFailure>(this, _$identity);
+
+  @override
+  @optionalTypeArgs
+  Result when<Result extends Object>({
+    @required Result initial(),
+    @required Result actionInProgress(),
+    @required Result deleteFailure(NoteFailure noteFailure),
+    @required Result deleteSuccess(),
+  }) {
+    assert(initial != null);
+    assert(actionInProgress != null);
+    assert(deleteFailure != null);
+    assert(deleteSuccess != null);
+    return deleteFailure(noteFailure);
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeWhen<Result extends Object>({
+    Result initial(),
+    Result actionInProgress(),
+    Result deleteFailure(NoteFailure noteFailure),
+    Result deleteSuccess(),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (deleteFailure != null) {
+      return deleteFailure(noteFailure);
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  Result map<Result extends Object>({
+    @required Result initial(_Initial value),
+    @required Result actionInProgress(_ActionInProgress value),
+    @required Result deleteFailure(_DeleteFailure value),
+    @required Result deleteSuccess(_DeleteSucess value),
+  }) {
+    assert(initial != null);
+    assert(actionInProgress != null);
+    assert(deleteFailure != null);
+    assert(deleteSuccess != null);
+    return deleteFailure(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeMap<Result extends Object>({
+    Result initial(_Initial value),
+    Result actionInProgress(_ActionInProgress value),
+    Result deleteFailure(_DeleteFailure value),
+    Result deleteSuccess(_DeleteSucess value),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (deleteFailure != null) {
+      return deleteFailure(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _DeleteFailure implements NoteActorState {
+  const factory _DeleteFailure(NoteFailure noteFailure) = _$_DeleteFailure;
+
+  NoteFailure get noteFailure;
+  _$DeleteFailureCopyWith<_DeleteFailure> get copyWith;
+}
+
+abstract class _$DeleteSucessCopyWith<$Res> {
+  factory _$DeleteSucessCopyWith(
+          _DeleteSucess value, $Res Function(_DeleteSucess) then) =
+      __$DeleteSucessCopyWithImpl<$Res>;
+}
+
+class __$DeleteSucessCopyWithImpl<$Res>
+    extends _$NoteActorStateCopyWithImpl<$Res>
+    implements _$DeleteSucessCopyWith<$Res> {
+  __$DeleteSucessCopyWithImpl(
+      _DeleteSucess _value, $Res Function(_DeleteSucess) _then)
+      : super(_value, (v) => _then(v as _DeleteSucess));
+
+  @override
+  _DeleteSucess get _value => super._value as _DeleteSucess;
+}
+
+class _$_DeleteSucess implements _DeleteSucess {
+  const _$_DeleteSucess();
+
+  @override
+  String toString() {
+    return 'NoteActorState.deleteSuccess()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) || (other is _DeleteSucess);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  Result when<Result extends Object>({
+    @required Result initial(),
+    @required Result actionInProgress(),
+    @required Result deleteFailure(NoteFailure noteFailure),
+    @required Result deleteSuccess(),
+  }) {
+    assert(initial != null);
+    assert(actionInProgress != null);
+    assert(deleteFailure != null);
+    assert(deleteSuccess != null);
+    return deleteSuccess();
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeWhen<Result extends Object>({
+    Result initial(),
+    Result actionInProgress(),
+    Result deleteFailure(NoteFailure noteFailure),
+    Result deleteSuccess(),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (deleteSuccess != null) {
+      return deleteSuccess();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  Result map<Result extends Object>({
+    @required Result initial(_Initial value),
+    @required Result actionInProgress(_ActionInProgress value),
+    @required Result deleteFailure(_DeleteFailure value),
+    @required Result deleteSuccess(_DeleteSucess value),
+  }) {
+    assert(initial != null);
+    assert(actionInProgress != null);
+    assert(deleteFailure != null);
+    assert(deleteSuccess != null);
+    return deleteSuccess(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeMap<Result extends Object>({
+    Result initial(_Initial value),
+    Result actionInProgress(_ActionInProgress value),
+    Result deleteFailure(_DeleteFailure value),
+    Result deleteSuccess(_DeleteSucess value),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (deleteSuccess != null) {
+      return deleteSuccess(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _DeleteSucess implements NoteActorState {
+  const factory _DeleteSucess() = _$_DeleteSucess;
 }
