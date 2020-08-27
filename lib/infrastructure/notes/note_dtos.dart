cd996964ea11945f6441237bae43dc9ecfd4c065
Braden Donaldson <bsdonald7@gmail.com>
2020-08-27 12:21:07 -0700

    started work on note domain transfer object. set up fromdomain and fromjson

--- lib/infrastructure/notes/note_dtos.dart
+++ lib/infrastructure/notes/note_dtos.dart
@@ -1,13 +1,51 @@
+import 'dart:ui';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:shattered_star/domain/core/value_objects.dart';
+import 'package:shattered_star/domain/notes/note.dart';
 import 'package:shattered_star/domain/notes/todo_item.dart';
 import 'package:shattered_star/domain/notes/value_objects.dart';
+import 'package:kt_dart/kt.dart';
 
 part 'note_dtos.freezed.dart';
 part 'note_dtos.g.dart';
 
 @freezed
-abstract class TodoItemDto implements _$TodoItemDto{
+abstract class NoteDto implements _$NoteDto {
+  const NoteDto._();
+
+  const factory NoteDto({
+    @JsonKey(ignore: true) String id,
+    @required String body,
+    @required int color,
+    @required List<TodoItemDto> todos,
+    @required FieldValue serverTimeStamp,
+  }) = _NoteDto;
+
+  factory NoteDto.fromDomain(Note note) {
+    return NoteDto(
+      id: note.id.getOrCrash(),
+      body: note.body.getOrCrash(),
+      color: note.color.getOrCrash().value,
+      todos: note.todos.getOrCrash().map((todoItem) => TodoItemDto.fromDomain(todoItem)).asList(),
+      serverTimeStamp: FieldValue.serverTimestamp(),
+    );
+  }
+
+  // Note toDomain() {
+  //   return Note(
+  //     id: UniqueId.fromUniqueString(id),
+  //     name: TodoName(name),
+  //     todos: todos,
+  //   );
+  // }
+
+  factory NoteDto.fromJson(Map<String, dynamic> json) => _$NoteDtoFromJson(json);
+}
+
+@freezed
+abstract class TodoItemDto implements _$TodoItemDto {
   const TodoItemDto._();
 
   const factory TodoItemDto({
@@ -22,15 +60,15 @@ abstract class TodoItemDto implements _$TodoItemDto{
       name: todoItem.name.getOrCrash(),
       done: todoItem.done,
     );
-  } 
+  }
 
   TodoItem toDomain() {
     return TodoItem(
       id: UniqueId.fromUniqueString(id),
       name: TodoName(name),
-      done: done, 
+      done: done,
     );
   }
 
   factory TodoItemDto.fromJson(Map<String, dynamic> json) => _$TodoItemDtoFromJson(json);
-} 
+}
