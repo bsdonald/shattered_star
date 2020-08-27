e8ad651bc2a8fae3c48ff9c866b2308b8acf8c14
Braden Donaldson <bsdonald7@gmail.com>
2020-08-27 11:00:51 -0700

    added CUD methods to the note repository

--- lib/domain/notes/i_note_repository.dart
+++ lib/domain/notes/i_note_repository.dart
@@ -1,15 +1,19 @@
 import 'package:dartz/dartz.dart';
 import 'package:kt_dart/collection.dart';
 import 'package:shattered_star/domain/notes/note.dart';
+import 'package:shattered_star/domain/notes/note_failure.dart';
 
-import 'note_failure.dart';
 
 abstract class INoteRepository {
-  //watch notes
-  //watch uncompleted notes
   //CUD
 
   //CRUD
 
+  //watch notes
   Stream<Either<NoteFailure, KtList<Note>>> watchAll();
+  //watch uncompleted notes
+  Stream<Either<NoteFailure, KtList<Note>>> watchUncompleted();
+  Future<Either<NoteFailure, Unit>> create(Note note);
+  Future<Either<NoteFailure, Unit>>update(Note note);
+  Future<Either<NoteFailure, Unit>> delete(Note note);
 }
