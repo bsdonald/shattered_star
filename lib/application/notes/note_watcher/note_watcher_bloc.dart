57a5cafab48121bde85d8cb077e97eeae02477c0
Braden Donaldson <bsdonald7@gmail.com>
2020-08-27 15:16:35 -0700

    set up note watcher state

--- lib/application/notes/note_watcher/note_watcher_bloc.dart
+++ lib/application/notes/note_watcher/note_watcher_bloc.dart
@@ -2,6 +2,9 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:kt_dart/collection.dart';
+import 'package:shattered_star/domain/notes/note.dart';
+import 'package:shattered_star/domain/notes/note_failure.dart';
 
 part 'note_watcher_event.dart';
 part 'note_watcher_state.dart';
--- lib/application/notes/note_watcher/note_watcher_bloc.freezed.dart
+++ lib/application/notes/note_watcher/note_watcher_bloc.freezed.dart
@@ -78,12 +78,59 @@ class _$NoteWatcherStateTearOff {
   _Initial initial() {
     return const _Initial();
   }
+
+  _LoadingInProgress loadingInProgress() {
+    return const _LoadingInProgress();
+  }
+
+  _LoadSuccess loadSuccess(KtList<Note> notes) {
+    return _LoadSuccess(
+      notes,
+    );
+  }
+
+  _LoadFailure loadFailure(NoteFailure noteFailure) {
+    return _LoadFailure(
+      noteFailure,
+    );
+  }
 }
 
 // ignore: unused_element
 const $NoteWatcherState = _$NoteWatcherStateTearOff();
 
-mixin _$NoteWatcherState {}
+mixin _$NoteWatcherState {
+  @optionalTypeArgs
+  Result when<Result extends Object>({
+    @required Result initial(),
+    @required Result loadingInProgress(),
+    @required Result loadSuccess(KtList<Note> notes),
+    @required Result loadFailure(NoteFailure noteFailure),
+  });
+  @optionalTypeArgs
+  Result maybeWhen<Result extends Object>({
+    Result initial(),
+    Result loadingInProgress(),
+    Result loadSuccess(KtList<Note> notes),
+    Result loadFailure(NoteFailure noteFailure),
+    @required Result orElse(),
+  });
+  @optionalTypeArgs
+  Result map<Result extends Object>({
+    @required Result initial(_Initial value),
+    @required Result loadingInProgress(_LoadingInProgress value),
+    @required Result loadSuccess(_LoadSuccess value),
+    @required Result loadFailure(_LoadFailure value),
+  });
+  @optionalTypeArgs
+  Result maybeMap<Result extends Object>({
+    Result initial(_Initial value),
+    Result loadingInProgress(_LoadingInProgress value),
+    Result loadSuccess(_LoadSuccess value),
+    Result loadFailure(_LoadFailure value),
+    @required Result orElse(),
+  });
+}
 
 abstract class $NoteWatcherStateCopyWith<$Res> {
   factory $NoteWatcherStateCopyWith(
@@ -129,8 +176,431 @@ class _$_Initial implements _Initial {
 
   @override
   int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  Result when<Result extends Object>({
+    @required Result initial(),
+    @required Result loadingInProgress(),
+    @required Result loadSuccess(KtList<Note> notes),
+    @required Result loadFailure(NoteFailure noteFailure),
+  }) {
+    assert(initial != null);
+    assert(loadingInProgress != null);
+    assert(loadSuccess != null);
+    assert(loadFailure != null);
+    return initial();
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeWhen<Result extends Object>({
+    Result initial(),
+    Result loadingInProgress(),
+    Result loadSuccess(KtList<Note> notes),
+    Result loadFailure(NoteFailure noteFailure),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (initial != null) {
+      return initial();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  Result map<Result extends Object>({
+    @required Result initial(_Initial value),
+    @required Result loadingInProgress(_LoadingInProgress value),
+    @required Result loadSuccess(_LoadSuccess value),
+    @required Result loadFailure(_LoadFailure value),
+  }) {
+    assert(initial != null);
+    assert(loadingInProgress != null);
+    assert(loadSuccess != null);
+    assert(loadFailure != null);
+    return initial(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeMap<Result extends Object>({
+    Result initial(_Initial value),
+    Result loadingInProgress(_LoadingInProgress value),
+    Result loadSuccess(_LoadSuccess value),
+    Result loadFailure(_LoadFailure value),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (initial != null) {
+      return initial(this);
+    }
+    return orElse();
+  }
 }
 
 abstract class _Initial implements NoteWatcherState {
   const factory _Initial() = _$_Initial;
+}
+
+abstract class _$LoadingInProgressCopyWith<$Res> {
+  factory _$LoadingInProgressCopyWith(
+          _LoadingInProgress value, $Res Function(_LoadingInProgress) then) =
+      __$LoadingInProgressCopyWithImpl<$Res>;
+}
+
+class __$LoadingInProgressCopyWithImpl<$Res>
+    extends _$NoteWatcherStateCopyWithImpl<$Res>
+    implements _$LoadingInProgressCopyWith<$Res> {
+  __$LoadingInProgressCopyWithImpl(
+      _LoadingInProgress _value, $Res Function(_LoadingInProgress) _then)
+      : super(_value, (v) => _then(v as _LoadingInProgress));
+
+  @override
+  _LoadingInProgress get _value => super._value as _LoadingInProgress;
+}
+
+class _$_LoadingInProgress implements _LoadingInProgress {
+  const _$_LoadingInProgress();
+
+  @override
+  String toString() {
+    return 'NoteWatcherState.loadingInProgress()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) || (other is _LoadingInProgress);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  Result when<Result extends Object>({
+    @required Result initial(),
+    @required Result loadingInProgress(),
+    @required Result loadSuccess(KtList<Note> notes),
+    @required Result loadFailure(NoteFailure noteFailure),
+  }) {
+    assert(initial != null);
+    assert(loadingInProgress != null);
+    assert(loadSuccess != null);
+    assert(loadFailure != null);
+    return loadingInProgress();
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeWhen<Result extends Object>({
+    Result initial(),
+    Result loadingInProgress(),
+    Result loadSuccess(KtList<Note> notes),
+    Result loadFailure(NoteFailure noteFailure),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (loadingInProgress != null) {
+      return loadingInProgress();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  Result map<Result extends Object>({
+    @required Result initial(_Initial value),
+    @required Result loadingInProgress(_LoadingInProgress value),
+    @required Result loadSuccess(_LoadSuccess value),
+    @required Result loadFailure(_LoadFailure value),
+  }) {
+    assert(initial != null);
+    assert(loadingInProgress != null);
+    assert(loadSuccess != null);
+    assert(loadFailure != null);
+    return loadingInProgress(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeMap<Result extends Object>({
+    Result initial(_Initial value),
+    Result loadingInProgress(_LoadingInProgress value),
+    Result loadSuccess(_LoadSuccess value),
+    Result loadFailure(_LoadFailure value),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (loadingInProgress != null) {
+      return loadingInProgress(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _LoadingInProgress implements NoteWatcherState {
+  const factory _LoadingInProgress() = _$_LoadingInProgress;
+}
+
+abstract class _$LoadSuccessCopyWith<$Res> {
+  factory _$LoadSuccessCopyWith(
+          _LoadSuccess value, $Res Function(_LoadSuccess) then) =
+      __$LoadSuccessCopyWithImpl<$Res>;
+  $Res call({KtList<Note> notes});
+}
+
+class __$LoadSuccessCopyWithImpl<$Res>
+    extends _$NoteWatcherStateCopyWithImpl<$Res>
+    implements _$LoadSuccessCopyWith<$Res> {
+  __$LoadSuccessCopyWithImpl(
+      _LoadSuccess _value, $Res Function(_LoadSuccess) _then)
+      : super(_value, (v) => _then(v as _LoadSuccess));
+
+  @override
+  _LoadSuccess get _value => super._value as _LoadSuccess;
+
+  @override
+  $Res call({
+    Object notes = freezed,
+  }) {
+    return _then(_LoadSuccess(
+      notes == freezed ? _value.notes : notes as KtList<Note>,
+    ));
+  }
+}
+
+class _$_LoadSuccess implements _LoadSuccess {
+  const _$_LoadSuccess(this.notes) : assert(notes != null);
+
+  @override
+  final KtList<Note> notes;
+
+  @override
+  String toString() {
+    return 'NoteWatcherState.loadSuccess(notes: $notes)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _LoadSuccess &&
+            (identical(other.notes, notes) ||
+                const DeepCollectionEquality().equals(other.notes, notes)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^ const DeepCollectionEquality().hash(notes);
+
+  @override
+  _$LoadSuccessCopyWith<_LoadSuccess> get copyWith =>
+      __$LoadSuccessCopyWithImpl<_LoadSuccess>(this, _$identity);
+
+  @override
+  @optionalTypeArgs
+  Result when<Result extends Object>({
+    @required Result initial(),
+    @required Result loadingInProgress(),
+    @required Result loadSuccess(KtList<Note> notes),
+    @required Result loadFailure(NoteFailure noteFailure),
+  }) {
+    assert(initial != null);
+    assert(loadingInProgress != null);
+    assert(loadSuccess != null);
+    assert(loadFailure != null);
+    return loadSuccess(notes);
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeWhen<Result extends Object>({
+    Result initial(),
+    Result loadingInProgress(),
+    Result loadSuccess(KtList<Note> notes),
+    Result loadFailure(NoteFailure noteFailure),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (loadSuccess != null) {
+      return loadSuccess(notes);
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  Result map<Result extends Object>({
+    @required Result initial(_Initial value),
+    @required Result loadingInProgress(_LoadingInProgress value),
+    @required Result loadSuccess(_LoadSuccess value),
+    @required Result loadFailure(_LoadFailure value),
+  }) {
+    assert(initial != null);
+    assert(loadingInProgress != null);
+    assert(loadSuccess != null);
+    assert(loadFailure != null);
+    return loadSuccess(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeMap<Result extends Object>({
+    Result initial(_Initial value),
+    Result loadingInProgress(_LoadingInProgress value),
+    Result loadSuccess(_LoadSuccess value),
+    Result loadFailure(_LoadFailure value),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (loadSuccess != null) {
+      return loadSuccess(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _LoadSuccess implements NoteWatcherState {
+  const factory _LoadSuccess(KtList<Note> notes) = _$_LoadSuccess;
+
+  KtList<Note> get notes;
+  _$LoadSuccessCopyWith<_LoadSuccess> get copyWith;
+}
+
+abstract class _$LoadFailureCopyWith<$Res> {
+  factory _$LoadFailureCopyWith(
+          _LoadFailure value, $Res Function(_LoadFailure) then) =
+      __$LoadFailureCopyWithImpl<$Res>;
+  $Res call({NoteFailure noteFailure});
+
+  $NoteFailureCopyWith<$Res> get noteFailure;
+}
+
+class __$LoadFailureCopyWithImpl<$Res>
+    extends _$NoteWatcherStateCopyWithImpl<$Res>
+    implements _$LoadFailureCopyWith<$Res> {
+  __$LoadFailureCopyWithImpl(
+      _LoadFailure _value, $Res Function(_LoadFailure) _then)
+      : super(_value, (v) => _then(v as _LoadFailure));
+
+  @override
+  _LoadFailure get _value => super._value as _LoadFailure;
+
+  @override
+  $Res call({
+    Object noteFailure = freezed,
+  }) {
+    return _then(_LoadFailure(
+      noteFailure == freezed ? _value.noteFailure : noteFailure as NoteFailure,
+    ));
+  }
+
+  @override
+  $NoteFailureCopyWith<$Res> get noteFailure {
+    if (_value.noteFailure == null) {
+      return null;
+    }
+    return $NoteFailureCopyWith<$Res>(_value.noteFailure, (value) {
+      return _then(_value.copyWith(noteFailure: value));
+    });
+  }
+}
+
+class _$_LoadFailure implements _LoadFailure {
+  const _$_LoadFailure(this.noteFailure) : assert(noteFailure != null);
+
+  @override
+  final NoteFailure noteFailure;
+
+  @override
+  String toString() {
+    return 'NoteWatcherState.loadFailure(noteFailure: $noteFailure)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _LoadFailure &&
+            (identical(other.noteFailure, noteFailure) ||
+                const DeepCollectionEquality()
+                    .equals(other.noteFailure, noteFailure)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^ const DeepCollectionEquality().hash(noteFailure);
+
+  @override
+  _$LoadFailureCopyWith<_LoadFailure> get copyWith =>
+      __$LoadFailureCopyWithImpl<_LoadFailure>(this, _$identity);
+
+  @override
+  @optionalTypeArgs
+  Result when<Result extends Object>({
+    @required Result initial(),
+    @required Result loadingInProgress(),
+    @required Result loadSuccess(KtList<Note> notes),
+    @required Result loadFailure(NoteFailure noteFailure),
+  }) {
+    assert(initial != null);
+    assert(loadingInProgress != null);
+    assert(loadSuccess != null);
+    assert(loadFailure != null);
+    return loadFailure(noteFailure);
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeWhen<Result extends Object>({
+    Result initial(),
+    Result loadingInProgress(),
+    Result loadSuccess(KtList<Note> notes),
+    Result loadFailure(NoteFailure noteFailure),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (loadFailure != null) {
+      return loadFailure(noteFailure);
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  Result map<Result extends Object>({
+    @required Result initial(_Initial value),
+    @required Result loadingInProgress(_LoadingInProgress value),
+    @required Result loadSuccess(_LoadSuccess value),
+    @required Result loadFailure(_LoadFailure value),
+  }) {
+    assert(initial != null);
+    assert(loadingInProgress != null);
+    assert(loadSuccess != null);
+    assert(loadFailure != null);
+    return loadFailure(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeMap<Result extends Object>({
+    Result initial(_Initial value),
+    Result loadingInProgress(_LoadingInProgress value),
+    Result loadSuccess(_LoadSuccess value),
+    Result loadFailure(_LoadFailure value),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (loadFailure != null) {
+      return loadFailure(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _LoadFailure implements NoteWatcherState {
+  const factory _LoadFailure(NoteFailure noteFailure) = _$_LoadFailure;
+
+  NoteFailure get noteFailure;
+  _$LoadFailureCopyWith<_LoadFailure> get copyWith;
 }
