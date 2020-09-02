c87f7463ad2b38c2e134cd01da57d04fa07c43c9
Braden Donaldson <bsdonald7@gmail.com>
2020-09-01 19:01:35 -0700

    set up stream to yield notes for the user

--- lib/infrastructure/notes/note_repository.dart
+++ lib/infrastructure/notes/note_repository.dart
@@ -1,20 +1,32 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:injectable/injectable.dart';
-import 'package:kt_dart/src/collection/kt_list.dart';
 import 'package:dartz/dartz.dart';
+import 'package:kt_dart/kt.dart';
 import 'package:shattered_star/domain/notes/i_note_repository.dart';
 import 'package:shattered_star/domain/notes/note_failure.dart';
 import 'package:shattered_star/domain/notes/note.dart';
+import 'package:shattered_star/infrastructure/core/firestore_helpers.dart';
+import 'package:shattered_star/infrastructure/notes/note_dtos.dart';
 
 @LazySingleton(as: INoteRepository)
 class NoteRepository implements INoteRepository {
-
   final Firestore _firestore;
 
   NoteRepository(this._firestore);
   @override
-  Stream<Either<NoteFailure, KtList<Note>>> watchAll() {
-    // TODO: implement watchAll
+  Stream<Either<NoteFailure, KtList<Note>>> watchAll() async* {
+    final userDoc = await _firestore.userDocument();
+    yield* userDoc.noteCollection
+        .orderBy(
+          'serverTimeStamp',
+          descending: true,
+        )
+        .snapshots()
+        .map(
+          (snapshot) => right(
+            snapshot.documents.map((doc) => NoteDto.fromFirestore(doc).toDomain()).toImmutableList(),
+          ),
+        );
     throw UnimplementedError();
   }
 
@@ -26,18 +38,18 @@ class NoteRepository implements INoteRepository {
 
   @override
   Future<Either<NoteFailure, Unit>> create(Note note) {
-      // TODO: implement create
-      throw UnimplementedError();
-    }
-  
-    @override
-    Future<Either<NoteFailure, Unit>> delete(Note note) {
-      // TODO: implement delete
-      throw UnimplementedError();
-    }
-  
-    @override
-    Future<Either<NoteFailure, Unit>> update(Note note) {
+    // TODO: implement create
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<NoteFailure, Unit>> delete(Note note) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<NoteFailure, Unit>> update(Note note) {
     // TODO: implement update
     throw UnimplementedError();
   }
