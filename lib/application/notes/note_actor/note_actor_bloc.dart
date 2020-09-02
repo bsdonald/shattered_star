de8a66f5aab7ae4e11df6b867681406ee4abb4b0
Braden Donaldson <bsdonald7@gmail.com>
2020-09-01 17:45:13 -0700

    set up note actor bloc

--- lib/application/notes/note_actor/note_actor_bloc.dart
+++ lib/application/notes/note_actor/note_actor_bloc.dart
@@ -2,6 +2,9 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+import 'package:meta/meta.dart';
+import 'package:shattered_star/domain/notes/i_note_repository.dart';
 import 'package:shattered_star/domain/notes/note.dart';
 import 'package:shattered_star/domain/notes/note_failure.dart';
 
@@ -9,13 +12,21 @@ part 'note_actor_event.dart';
 part 'note_actor_state.dart';
 part 'note_actor_bloc.freezed.dart';
 
+@injectable
 class NoteActorBloc extends Bloc<NoteActorEvent, NoteActorState> {
-  NoteActorBloc() : super(_Initial());
+  final INoteRepository _noteRepository;
+  
+  NoteActorBloc(this._noteRepository) : super(const NoteActorState.initial());
 
   @override
   Stream<NoteActorState> mapEventToState(
     NoteActorEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    yield const NoteActorState.actionInProgress();
+    final possibleFailure = await _noteRepository.delete(event.note);
+    yield possibleFailure.fold(
+      (f) => NoteActorState.deleteFailure(f),
+      (_) => const NoteActorState.deleteSuccess(),
+    );
   }
 }
