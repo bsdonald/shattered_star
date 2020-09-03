cd65cce15ee233f2ef7c3c51563f4b63392d3fc1
Braden Donaldson <bsdonald7@gmail.com>
2020-09-03 11:47:54 -0700

    mapped colorchanged to state

--- lib/application/notes/note_form/note_form_bloc.dart
+++ lib/application/notes/note_form/note_form_bloc.dart
@@ -8,6 +8,7 @@ import 'package:kt_dart/kt.dart';
 import 'package:shattered_star/domain/notes/i_note_repository.dart';
 import 'package:shattered_star/domain/notes/note.dart';
 import 'package:shattered_star/domain/notes/note_failure.dart';
+import 'package:shattered_star/domain/notes/value_objects.dart';
 import 'package:shattered_star/presentation/notes/note_form/misc/todo_item_presentation_classes.dart';
 
 part 'note_form_event.dart';
@@ -34,9 +35,17 @@ class NoteFormBloc extends Bloc<NoteFormEvent, NoteFormState> {
         );
       },
       bodyChanged: (e) async* {
-        yield state.copyWith
+        yield state.copyWith(
+          note: state.note.copyWith(body: NoteBody(e.bodyStr)),
+          saveFailureOrSuccessOption: none(),
+        );
       },
-      colorChanged: (e) async* {},
+      colorChanged: (e) async* {
+        yield state.copyWith(
+          note: state.note.copyWith(color: NoteColor(e.color)),
+          saveFailureOrSuccessOption: none(),
+        );
+      },
       todosChanged: (e) async* {},
       saved: (e) async* {},
     );
