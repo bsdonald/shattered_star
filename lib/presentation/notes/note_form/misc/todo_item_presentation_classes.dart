e25d424d1f9bf7d66e429db4de09342431dc6c6d
Braden Donaldson <bsdonald7@gmail.com>
2020-09-03 10:20:01 -0700

    finished setting up todo item primitive

--- lib/presentation/notes/note_form/misc/todo_item_presentation_classes.dart
+++ lib/presentation/notes/note_form/misc/todo_item_presentation_classes.dart
@@ -1,6 +1,15 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:shattered_star/domain/core/value_objects.dart';
+
+part 'todo_item_presentation_classes.freezed.dart';
 
 @freezed
-abstract class TodoItemPrimitive implements _$TodoItemPrimitive(
-  const factory TodoItemPrimitive() _TodoItemPrimitive;
-)
+abstract class TodoItemPrimitive implements _$TodoItemPrimitive {
+  const TodoItemPrimitive._();
+
+  const factory TodoItemPrimitive({
+    @required UniqueId id,
+    @required String name,
+    @required bool done,
+  }) = _TodoItemPrimitive;
+}
