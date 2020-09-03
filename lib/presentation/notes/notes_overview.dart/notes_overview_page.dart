ebb24ac15b7699f98c14d8194e49a17ae802f0bf
Braden Donaldson <bsdonald7@gmail.com>
2020-09-03 16:55:02 -0700

    set up multiblocprovider

--- lib/presentation/notes/notes_overview.dart/notes_overview_page.dart
+++ lib/presentation/notes/notes_overview.dart/notes_overview_page.dart
@@ -1,31 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shattered_star/application/auth/auth_bloc.dart';
+import 'package:shattered_star/application/notes/note_actor/note_actor_bloc.dart';
+import 'package:shattered_star/application/notes/note_watcher/note_watcher_bloc.dart';
+import 'package:shattered_star/injection.dart';
 
 class NotesOverviewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Notes'),
-        leading: IconButton(
-          icon: Icon(Icons.exit_to_app),
-          onPressed: () {
-            context.bloc<AuthBloc>().add(const AuthEvent.signedOut());
-          },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<NoteWatcherBloc>(
+          create: (context) => getIt<NoteWatcherBloc>()..add(const NoteWatcherEvent.watchAllStarted()),
         ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.indeterminate_check_box),
-            onPressed: () {},
+        BlocProvider<NoteActorBloc>(
+          create: (context) => getIt<NoteActorBloc>(),
+        ),
+      ],
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Notes'),
+          leading: IconButton(
+            icon: Icon(Icons.exit_to_app),
+            onPressed: () {
+              context.bloc<AuthBloc>().add(const AuthEvent.signedOut());
+            },
           ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          //TODO: Navigate to NoteFormPage
-        },
-        child: Icon(Icons.add),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.indeterminate_check_box),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        // body: ,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            //TODO: Navigate to NoteFormPage
+          },
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
