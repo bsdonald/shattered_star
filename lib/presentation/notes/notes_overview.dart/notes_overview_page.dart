690f06fa57ac9510ade1d7719bbecae349657cf6
Braden Donaldson <bsdonald7@gmail.com>
2020-09-17 10:27:51 -0700

    implemented sign out button

--- lib/presentation/notes/notes_overview.dart/notes_overview_page.dart
+++ lib/presentation/notes/notes_overview.dart/notes_overview_page.dart
@@ -1,9 +1,11 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shattered_star/application/auth/auth_bloc.dart';
 import 'package:shattered_star/application/notes/note_actor/note_actor_bloc.dart';
 import 'package:shattered_star/application/notes/note_watcher/note_watcher_bloc.dart';
 import 'package:shattered_star/injection.dart';
+import 'package:shattered_star/presentation/routes/router.gr.dart';
 
 class NotesOverviewPage extends StatelessWidget {
   @override
@@ -17,28 +19,39 @@ class NotesOverviewPage extends StatelessWidget {
           create: (context) => getIt<NoteActorBloc>(),
         ),
       ],
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Notes'),
-          leading: IconButton(
-            icon: Icon(Icons.exit_to_app),
-            onPressed: () {
-              context.bloc<AuthBloc>().add(const AuthEvent.signedOut());
-            },
-          ),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.indeterminate_check_box),
-              onPressed: () {},
+      child: MultiBlocListener(
+        listeners: [
+          BlocListener<AuthBloc, AuthState>(
+            listener: (context, state) {
+              state.maybeMap(
+                unauthenticated: (_) => ExtendedNavigator.of(context).replace(Routes.signInPage),
+                orElse: () {},);
+            }
+          )
+        ],
+              child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Notes'),
+            leading: IconButton(
+              icon: Icon(Icons.exit_to_app),
+              onPressed: () {
+                context.bloc<AuthBloc>().add(const AuthEvent.signedOut());
+              },
             ),
-          ],
-        ),
-        // body: ,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            //TODO: Navigate to NoteFormPage
-          },
-          child: Icon(Icons.add),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.indeterminate_check_box),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          // body: ,
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              //TODO: Navigate to NoteFormPage
+            },
+            child: Icon(Icons.add),
+          ),
         ),
       ),
     );
